de04ddf4c0adc97ddaf6a3a104b56dc607394c77
norsween <norsween@gmail.com>
2018-05-05 00:39:24 +0100

    add code to set username to display in chat rooms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
+import User from './components/User';
 
 // Initialize Firebase
 var config = {
@@ -17,14 +18,19 @@ firebase.initializeApp(config);
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       activeRoom: null,
+      user: null
     };
   }
   
   setActiveRoom(room) {
     this.setState({activeRoom: room});
+  }
+
+  setUser(user) {
+    this.setState({user: user});
   }
   
   render() {
@@ -33,6 +39,9 @@ class App extends Component {
         <aside id="sidebar">
           <h1 className="App-title">Bloc Chat</h1>
           <RoomList firebase={firebase} activeRoom={this.state.activeRoom} setActiveRoom={this.setActiveRoom.bind(this)} />
+        </aside>
+        <aside id="sidebar-bottom">
+           <User firebase={firebase} user={this.state.user} setUser={this.setUser.bind(this)} />
         </aside>
         <MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
       </div>
